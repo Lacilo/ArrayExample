b85ab629ee95a4e365f9724e609f9c80651684a0
Lacilo <szaszfail@kkszki.hu>
2025-09-18 10:25:59 +0200

    kommentelés

--- ArrayExample/Program.cs
+++ ArrayExample/Program.cs
@@ -9,26 +9,28 @@ namespace ArrayExample
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)                                 // FŐPROGRAM
         {
-            int[] nums = new int[5];
+            // LISTA LÉTREHOZÁSA
+            int[] nums = new int[5];                                    // 5 ELEM HOSSZÚ LISTA INICIALIZÁLÁSA
+
 
             // AZ ELEMEK BEKÉRÉSE EGYESÉVEL
             //nums[0] = int.Parse(Console.ReadLine());
 
 
             // AZ ADATOK BEKÉRÉSE CIKLUSSAL
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)                                 // 5 SZÁMOT BEKÉRŐ CIKLUS
             {
-                Console.Write($"Kérem a(z) {i + 1}. elemet \\-$ ");
-                nums[i] = int.Parse(Console.ReadLine());
+                Console.Write($"Kérem a(z) {i + 1}. elemet \\-$ ");     // LISTA ELEMEINEK BEKÉRÉSE
+                nums[i] = int.Parse(Console.ReadLine());                // ELTÁROLÁS A nums LISTÁBA
             }
 
 
             // AZ ADATOK KIÍRÁSA CIKLUSSAL
-            foreach (int num in nums)
+            foreach (int num in nums)                                   // VÉGIGLÉPKEDÉS A nums LISTÁN
             {
-                Console.Write(num + ", ");
+                Console.Write(num + ", ");                              // SZÁMOK EGY SORBA VALÓ KIÍRÁSA
             }
 
         }
