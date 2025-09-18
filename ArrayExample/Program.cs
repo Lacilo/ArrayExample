9e2a6183350c14abde01710793b519528123110f
Lacilo <szaszfail@kkszki.hu>
2025-09-18 10:23:24 +0200

    ciklusok létrehozása

--- ArrayExample/Program.cs
+++ ArrayExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace ArrayExample
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[5];
+
+            // AZ ELEMEK BEKÉRÉSE EGYESÉVEL
+            //nums[0] = int.Parse(Console.ReadLine());
+
+
+            // AZ ADATOK BEKÉRÉSE CIKLUSSAL
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Kérem a(z) {i + 1}. elemet \\-$ ");
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            // AZ ADATOK KIÍRÁSA CIKLUSSAL
+            foreach (int num in nums)
+            {
+                Console.Write(num + ", ");
+            }
 
         }
     }
